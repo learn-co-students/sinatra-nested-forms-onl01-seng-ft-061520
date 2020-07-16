5ca60ee263b906fc9dbe2de99f69d1dd2bdb8ae5
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-16 21:45:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,12 +4,13 @@ module FormsLab
   class App < Sinatra::Base
 
     get '/' do 
-      
       erb :root 
+      
     end 
     
     get '/new' do 
       erb :new
+      
     end 
     
     post '/pirates' do
